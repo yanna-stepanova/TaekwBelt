3b27bf691bbd577a4360f9114fe90834886c4a3b
Yana Stepanova <yanna.stepanova@gmail.com>
2022-09-14 21:47:49 +0300

    Test for filling data from json-file.

--- app/src/test/java/com/example/taekwbelt/models/UBTerminologyItemTest.java
+++ app/src/test/java/com/example/taekwbelt/models/UBTerminologyItemTest.java
@@ -1,9 +1,24 @@
 package com.example.taekwbelt.models;
 
 import static org.junit.Assert.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
 public class UBTerminologyItemTest {
+    @Test
+    public void testFillFromJson() throws JSONException {
+        UBTerminologyItem expectedObject = new UBTerminologyItem("0",
+                                                                 "Training Hall",
+                                                                 "Dojang");
+        String textJson = "{ \"id\": \"0\",\n \"English\": \"Training Hall\",\n \"Korean\": \"Dojang\"}";
+        JSONObject jsonObject = new JSONObject(textJson);
+        UBTerminologyItem actualObject = new UBTerminologyItem(jsonObject);
+        assertEquals(expectedObject.getIdentifier(), actualObject.getIdentifier());
+        assertEquals(expectedObject.getEnglish(), actualObject.getEnglish());
+        assertEquals(expectedObject.getKorean(), actualObject.getKorean());
+    }
 
     @Test
     public void testIdNull() {
